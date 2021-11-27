624dc9ae36589aaf55e5823eef74d3b661fda6c5
Travis Prosser <travisprosser@gmail.com>
2021-11-27 10:24:39 -0400

    fix: Tidy up configuration

---
+++ ark_discord_bot/config.py
@@ -0,0 +1,61 @@
+import os
+import logging
+import yaml
+from ark_discord_bot.utils import parse_list
+
+logger = logging.getLogger(__name__)
+
+
+DEFAULT_CONFIG = {
+    "allowed_roles": ["Administrator", "Tech Administrator", "dot"],
+    "polling_delay": 5,
+    "rates_url": None,
+    "bans_url": None,
+    "rates_channel_id": None,
+    "bans_channel_id": None,
+    "token": None,
+}
+
+REQUIRED_VALUES = [
+    "token",
+    "rates_channel_id",
+    "bans_channel_id",
+]
+
+
+def setup_config(args, default_config=DEFAULT_CONFIG):
+    # Attempt to load config values from file if provided
+    config_path = args.config_path or os.getenv("BOT_CONFIG_PATH")
+    logger.debug(f"CONFIG_PATH: {config_path}")
+    if config_path:
+        try:
+            with open(os.path.expanduser(config_path)) as f:
+                config_from_file = yaml.safe_load(f)
+        except FileNotFoundError as e:
+            logger.error(
+                f"Could not find config file at specified location: {config_path}"
+            )
+            exit(1)
+    else:
+        config_from_file = {}
+
+    # For each configuration value, attempt to obtain value in the specified order of priority:
+    config = {}
+    for key, default_val in default_config.items():
+        config[key] = (
+            getattr(args, key)
+            or config_from_file.get(key)
+            or os.getenv(f"BOT_{key.upper()}")
+            or default_val
+        )
+
+    # handle special case for list parsing
+    if isinstance(config["allowed_roles"], str):
+        try:
+            config["allowed_roles"] = parse_list(config["allowed_roles"])
+        except TypeError as e:
+            logger.warning(
+                f"Incorrect variable format; should be comma separated list: {e}"
+            )
+
+    return config
--- ark_discord_bot/main.py
+++ ark_discord_bot/main.py
@@ -1,12 +1,10 @@
 import discord
 from discord.ext import commands
-import yaml
 import ark_discord_bot
-import argparse
-import os
 import logging
-from ark_discord_bot.utils import parse_list
+from ark_discord_bot.utils import setup_argparse
 from ark_discord_bot.bot import ConfigurableBot
+from ark_discord_bot.config import setup_config, REQUIRED_VALUES
 
 # https://discord.com/api/oauth2/authorize?client_id=912847784477065278&permissions=117760&scope=bot//
 
@@ -24,62 +22,7 @@ def cli():
     logger.addHandler(ch)
 
     # Define CLI arguments.
-    parser = argparse.ArgumentParser(
-        description="""
-        Bot for sending messages to Discord whenever ARK Web API changes.
-        
-        This bot requires some configuration parameters to be set. They can either
-        be provided via a config file, CLI args, or environment variables. These
-        values are set using the following order of priority:
-
-            1. CLI Arguments
-            2. Configuration file
-            3. Environment Variables
-        """,
-        formatter_class=argparse.RawDescriptionHelpFormatter,
-    )
-    parser.add_argument(
-        "-c",
-        "--config-path",
-        type=str,
-        help="Path to configuration file (YAML). Also can be set via BOT_CONFIG_PATH environment variable.",
-    )
-    parser.add_argument(
-        "-t",
-        "--token",
-        type=str,
-        help="API token for ARK. Also can be set via BOT_TOKEN environment variable.",
-    )
-    parser.add_argument(
-        "-p",
-        "--polling-delay",
-        type=float,
-        default=5,
-        help="Polling period in seconds. Also can be set via BOT_POLLING_PERIOD environment variable.",
-    )
-    parser.add_argument(
-        "-a",
-        "--allowed-roles",
-        type=parse_list,
-        help="Comma-separated list of allowed roles. Also can be set via BOT_ALLOWED_ROLES environment variable.",
-    )
-    parser.add_argument(
-        "--rates-url",
-        help="URL for ARK rates. Also can be set via BOT_RATES_URL environment variable.",
-    )
-    parser.add_argument(
-        "--rates-channel-id",
-        help="Discord 'rates' channel ID. Also can be set via BOT_RATES_CHANNEL_ID environment variable.",
-    )
-    parser.add_argument(
-        "--bans-url",
-        help="URL for ARK bans. Also can be set via BOT_BANS_URL environment variable.",
-    )
-    parser.add_argument(
-        "--bans-channel-id",
-        help="Discord 'bans' channel ID. Also can be set via BOT_BANS_CHANNEL_ID environment variable.",
-    )
-    parser.add_argument("--debug", action="store_true", help="Set log level to DEBUG.")
+    parser = setup_argparse()
 
     # Load arguments from CLI
     args = parser.parse_args()
@@ -88,60 +31,16 @@ def cli():
     if args.debug:
         ch.setLevel(logging.DEBUG)
 
-    # Attempt to load config values from file if provided
-    config_path = args.config_path or os.getenv("BOT_CONFIG_PATH")
-    logger.debug(f"CONFIG_PATH: {config_path}")
-    if config_path:
-        try:
-            with open(os.path.expanduser(config_path)) as f:
-                config = yaml.safe_load(f)
-        except FileNotFoundError as e:
-            logger.error(
-                f"Could not find config file at specified location: {config_path}"
-            )
-            exit(1)
-    else:
-        config = {}
+    # Set up configuration dict
+    config = setup_config(args)
 
-    # For each configuration value, attempt to obtain value in the specified order of priority:
-    config["allowed_roles"] = (
-        args.allowed_roles
-        or config.get("allowed_roles")
-        or parse_list(os.getenv("BOT_ALLOWED_ROLES"))
-        if os.getenv("BOT_ALLOWED_ROLES")
-        else None or ["Administrator", "Tech Administrator", "dot"]
-    )
-    config["token"] = config.get("token") or args.token or os.getenv("BOT_TOKEN")
-    config["polling_delay"] = (
-        args.polling_delay
-        or config.get("polling_delay")
-        or os.getenv("BOT_POLLING_DELAY")
-    )
-    config["rates_url"] = (
-        args.rates_url
-        or config.get("rates_url")
-        or os.getenv("BOT_RATES_URL")
-        or "http://arkdedicated.com/dynamicconfig.ini"
-    )
-    config["rates_channel_id"] = (
-        args.rates_channel_id
-        or config.get("rates_channel_id")
-        or os.getenv("BOT_RATES_CHANNEL_ID")
-    )
-    config["bans_url"] = (
-        args.bans_url
-        or config.get("bans_url")
-        or os.getenv("BOT_BANS_URL")
-        or "http://arkdedicated.com/bansummary.txt"
-    )
-    config["bans_channel_id"] = (
-        args.bans_channel_id
-        or config.get("bans_channel_id")
-        or os.getenv("BOT_BANS_CHANNEL_ID")
-    )
-
-    if not config.get("token"):
-        logger.error("No Token specified.")
+    # Check for required values
+    missing_vals = []
+    for val in REQUIRED_VALUES:
+        if not config.get(val):
+            missing_vals.append(val)
+    if len(missing_vals) > 0:
+        logger.error(f"Missing required configuration variables: {missing_vals}")
         exit(1)
 
     run(**config)
