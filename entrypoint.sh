4b7fa040e844da9a9e898ff070a3a02f8675d5a6
Travis Prosser <travisprosser@gmail.com>
2021-11-27 14:26:20 -0400

    fix entrypoint script to pass args

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if touch $BOT_OUTPUT_DIR 2> /dev/null; then
-    ark-discord-bot
+    ark-discord-bot $@
 else 
     echo "Cannot write to data directory: ${BOT_OUTPUT_DIR}. Create data directory on host before running container."
     exit 1
