42b4623b099d5162c38d874ad3d18f917a0c1123
Travis Prosser <travisprosser@gmail.com>
2021-11-27 14:09:32 -0400

    fix: Add entrypoint script to help with bind mount permission error

---
+++ entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if touch $BOT_OUTPUT_DIR 2> /dev/null; then
+    ark-discord-bot
+else 
+    echo "Cannot write to data directory: ${BOT_OUTPUT_DIR}. Create data directory on host before running container."
+    exit 1
+fi 
