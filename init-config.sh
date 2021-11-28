da74a51946a684330e9c789e4f9c5e364b9c0881
Travis Prosser <travisprosser@gmail.com>
2021-11-28 19:01:38 -0400

    feat: Add scripts for init and running

--- init-config.sh
+++ init-config.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-# check if config dir exists; if not, create it
+DEFAULT_DIR="${HOME}/.ark-discord-bot"
+
+# Check if user set custom config dir; if not, use default
+if [ -z "${BOT_CONFIG_DIR}" ]; then 
+    echo "BOT_CONFIG_DIR not set; Assuming default location ${DEFAULT_DIR}"
+    BOT_CONFIG_DIR="${DEFAULT_DIR}"
+else
+    echo "Using custom config dir: BOT_CONFIG_DIR=${BOT_CONFIG_DIR}"
+fi
+
+# Check if config dir exists; if not, create it
+if [ ! -d "${BOT_CONFIG_DIR}" ]; then
+    echo "Directory ${BOT_CONFIG_DIR} doesn't exist; Creating."
+    mkdir -p "${BOT_CONFIG_DIR}"
+else   
+    echo "Directory exists: ${BOT_CONFIG_DIR}. Skipping creation."
+fi
