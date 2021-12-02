722209b7e270f38709430aa377878ca4f069d8dc
Travis Prosser <travisprosser@gmail.com>
2021-12-02 00:58:49 -0400

    test: Add CLI tests

---
+++ tests/unit/test_cli.py
@@ -0,0 +1,50 @@
+from ark_discord_bot.utils import setup_argparse, parse_list
+
+
+def test_parse_list():
+    assert parse_list("this,       is,  a  , test    ") == ["this", "is", "a", "test"]
+    assert parse_list("") == [""]
+    assert parse_list("this,has,a,double,,comma") == [
+        "this",
+        "has",
+        "a",
+        "double",
+        "",
+        "comma",
+    ]
+
+
+def test_parser():
+    parser = setup_argparse()
+
+    args = parser.parse_args(
+        [
+            "-d",
+            "~/data-dir",
+            "-t",
+            "MYTOKEN",
+            "-p",
+            "500",
+            "-a",
+            "test,admin",
+            "--rates-url",
+            "www.test.com",
+            "--rates-channel-id",
+            "1234",
+            "--bans-url",
+            "www.bans.com",
+            "--bans-channel-id",
+            "4321",
+            "--debug",
+        ]
+    )
+
+    assert args.data_dir == "~/data-dir"
+    assert args.token == "MYTOKEN"
+    assert args.polling_delay == 500
+    assert args.allowed_roles == ["test", "admin"]
+    assert args.rates_url == "www.test.com"
+    assert args.rates_channel_id == "1234"
+    assert args.bans_url == "www.bans.com"
+    assert args.bans_channel_id == "4321"
+    assert args.debug == True
