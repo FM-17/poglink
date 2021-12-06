de8d43fe9eb6e29c9d5b408f9b7b758859db367c
Travis Prosser <travisprosser@gmail.com>
2021-12-05 21:44:34 -0400

    ci: Fix scm_version commit hash issue

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 # TODO: Transition to setup.cfg https://setuptools.pypa.io/en/latest/userguide/quickstart.html#transitioning-from-setup-py-to-setup-cfg
 from setuptools import find_packages, setup
-import os 
+import os
 
 # If PYTHON_PACKAGE_VERSION variable is provided, use this instead of automatically grabbing from SCM.
-version=os.getenv("PYTHON_PACKAGE_VERSION")
-use_scm_version = False if version else True
+version = os.getenv("PYTHON_PACKAGE_VERSION")
+
+# the local scheme is overridden because pypi doesn't support commit hashes in version numbers
+if version:
+    use_scm_version = False
+elif os.getenv("CI") == "true":
+    use_scm_version = {"local_scheme": "no-local-version"}
+else:
+    use_scm_version = True
 
 
 setup(
