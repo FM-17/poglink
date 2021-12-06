e14a7eb4da7b7c19d288f8e847380e319b1078e8
Travis Prosser <travisprosser@gmail.com>
2021-12-06 01:38:36 -0400

    test: rename fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,30 +3,30 @@ import shutil
 import os
 
 
-SAMPLE_BANSUMMARY_PATH = "tests/data/bansummary-1.txt"
-SAMPLE_BANSUMMARY_CHANGED_PATH = "tests/data/bansummary-2.txt"
+SAMPLE_BANSUMMARY_PATH_1 = "tests/data/bansummary-1.txt"
+SAMPLE_BANSUMMARY_PATH_2 = "tests/data/bansummary-2.txt"
 LAST_BANSUMMARY_PATH = "tests/data/last_bans.txt"
 
-SAMPLE_DYNAMICCONFIG_PATH = "tests/data/dynamicconfig-1.ini"
-SAMPLE_CHANGED_DYNAMICCONFIG_PATH = "tests/data/dynamicconfig-2.ini"
+SAMPLE_DYNAMICCONFIG_PATH_1 = "tests/data/dynamicconfig-1.ini"
+SAMPLE_DYNAMICCONFIG_PATH_2 = "tests/data/dynamicconfig-2.ini"
 LAST_DYNAMICCONFIG_PATH = "tests/data/last_rates.txt"
 
 # bans
 @pytest.fixture
-def sample_bansummary_txt():
-    with open(SAMPLE_BANSUMMARY_PATH) as f:
+def sample_bansummary_1():
+    with open(SAMPLE_BANSUMMARY_PATH_1) as f:
         return f.read()
 
 
 @pytest.fixture
-def sample_bansummary_changed_ini():
-    with open(SAMPLE_BANSUMMARY_CHANGED_PATH) as f:
+def sample_bansummary_2():
+    with open(SAMPLE_BANSUMMARY_PATH_2) as f:
         return f.read()
 
 
 @pytest.fixture
 def last_bans():
-    shutil.copy(SAMPLE_BANSUMMARY_PATH, LAST_BANSUMMARY_PATH)
+    shutil.copy(SAMPLE_BANSUMMARY_PATH_1, LAST_BANSUMMARY_PATH)
     with open(LAST_BANSUMMARY_PATH) as f:
         yield f.read()
     os.remove(LAST_BANSUMMARY_PATH)
@@ -34,20 +34,20 @@ def last_bans():
 
 # rates
 @pytest.fixture
-def sample_dynamicconfig_ini():
-    with open(SAMPLE_DYNAMICCONFIG_PATH) as f:
+def sample_dynamicconfig_1():
+    with open(SAMPLE_DYNAMICCONFIG_PATH_1) as f:
         return f.read()
 
 
 @pytest.fixture
-def sample_dynamicconfig_changed_ini():
-    with open(SAMPLE_CHANGED_DYNAMICCONFIG_PATH) as f:
+def sample_dynamicconfig_2():
+    with open(SAMPLE_DYNAMICCONFIG_PATH_2) as f:
         return f.read()
 
 
 @pytest.fixture
 def last_rates():
-    shutil.copy(SAMPLE_DYNAMICCONFIG_PATH, LAST_DYNAMICCONFIG_PATH)
+    shutil.copy(SAMPLE_DYNAMICCONFIG_PATH_1, LAST_DYNAMICCONFIG_PATH)
     with open(LAST_DYNAMICCONFIG_PATH) as f:
         yield f.read()
     os.remove(LAST_DYNAMICCONFIG_PATH)
@@ -57,14 +57,14 @@ def last_rates():
 @pytest.fixture
 def configured_httpserver(
     httpserver,
-    sample_bansummary_txt,
-    sample_dynamicconfig_ini,
+    sample_bansummary_1,
+    sample_dynamicconfig_1,
 ):
     httpserver.expect_request("/bansummary.txt").respond_with_data(
-        sample_bansummary_txt, content_type="text/plain"
+        sample_bansummary_1, content_type="text/plain"
     )
     httpserver.expect_request("/dynamicconfig.ini").respond_with_data(
-        sample_dynamicconfig_ini, content_type="text/plain"
+        sample_dynamicconfig_1, content_type="text/plain"
     )
 
     return httpserver
--- tests/integration/test_async_http_request.py
+++ tests/integration/test_async_http_request.py
@@ -5,24 +5,24 @@ import pytest
 from ark_discord_bot.models import RatesStatus
 
 
-def test_bansummary_request(configured_httpserver, sample_bansummary_txt):
+def test_bansummary_request(configured_httpserver, sample_bansummary_1):
     resp = requests.get(configured_httpserver.url_for("/bansummary.txt"))
-    assert resp.text == sample_bansummary_txt
+    assert resp.text == sample_bansummary_1
 
 
 @pytest.mark.asyncio
-async def test_async_bansummary_request(configured_httpserver, sample_bansummary_txt):
+async def test_async_bansummary_request(configured_httpserver, sample_bansummary_1):
     async with aiohttp.ClientSession() as session:
         async with session.get(
             configured_httpserver.url_for("/bansummary.txt"),
             headers={"Pragma": "no-cache", "Cache-Control": "no-cache"},
         ) as resp:
             text = await resp.text()
-            assert text == sample_bansummary_txt
+            assert text == sample_bansummary_1
 
 
 @pytest.mark.asyncio
-async def test_rates_changed(configured_httpserver, sample_dynamicconfig_changed_ini):
+async def test_rates_changed(configured_httpserver, sample_dynamicconfig_2):
     async with aiohttp.ClientSession() as session:
         async with session.get(
             configured_httpserver.url_for("/dynamicconfig.ini"),
@@ -30,7 +30,7 @@ async def test_rates_changed(configured_httpserver, sample_dynamicconfig_changed
         ) as resp:
             text = await resp.text()
 
-    rates_diff = RatesStatus.from_raw(text).get_diff(RatesStatus.from_raw(sample_dynamicconfig_changed_ini))
+    rates_diff = RatesStatus.from_raw(text).get_diff(RatesStatus.from_raw(sample_dynamicconfig_2))
     assert len(rates_diff.items) == 3
     assert set([item.key for item in rates_diff.items]) == set(
         [
