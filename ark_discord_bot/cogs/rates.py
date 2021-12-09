1f5dac81e18383d1f537b14641e7c7efa58e9a08
Travis Prosser <travisprosser@gmail.com>
2021-12-09 01:10:21 -0400

    feat: Consider multiple rates urls when fetching; Name local files after url basename

--- ark_discord_bot/cogs/rates.py
+++ ark_discord_bot/cogs/rates.py
@@ -6,6 +6,7 @@ import asyncio
 import aiohttp
 import logging
 import yaml
+from urllib.parse import urlparse
 import os
 import json
 import copy
@@ -17,12 +18,18 @@ class Rates(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-        self.webpage_url = client.config.rates_url
+        self.webpage_urls = client.config.rates_urls
         self.channel_id = client.config.rates_channel_id
         self.polling_delay = client.config.polling_delay
         self.allowed_roles = client.config.allowed_roles
         self.data_dir = os.path.expanduser(client.config.data_dir)
-        self.output_path = os.path.join(self.data_dir, "last_rates.json")
+        self.output_paths = [
+            os.path.join(
+                self.data_dir,
+                f"last_{os.path.splitext(os.path.basename(urlparse(url).path))[0]}.json",
+            )
+            for url in client.config.rates_urls
+        ]
         self.keyMapping = {
             "TamingSpeedMultiplier": "Taming",
             "HarvestAmountMultiplier": "Harvesting",
@@ -39,10 +46,11 @@ class Rates(commands.Cog):
         if not os.path.exists(self.data_dir):
             os.makedirs(self.data_dir)
 
-    async def get_current_rates(self):
+    @staticmethod
+    async def get_current_rates(url):
         async with aiohttp.ClientSession() as session:
             async with session.get(
-                self.webpage_url,
+                url,
                 headers={"Pragma": "no-cache", "Cache-Control": "no-cache"},
             ) as response:
                 response = await response.text()
@@ -72,52 +80,52 @@ class Rates(commands.Cog):
         logger.info("Cog Ready: Rates")
 
         while True:
-
-            # get current rates from ARK Web API
-            logger.info("Retrieving current rates")
-            try:
-                rates = await self.get_current_rates()
-
-            except Exception as e:
-                logger.error(f"Could not retrieve rates from ARK Web API: {e}")
-                await asyncio.sleep(self.polling_delay)
-                continue
-
-            # get old rates from file
-            try:
-                with open(self.output_path) as f:
-                    last_rates_dict = json.load(f)
-            except (FileNotFoundError, JSONDecodeError) as e:
-                logger.warn(f"Problem loading file at {self.output_path}: {e}")
-                last_rates_dict = copy.deepcopy(rates).to_dict()
+            for url, output_path in zip(self.webpage_urls, self.output_paths):
+                # get current rates from ARK Web API
+                logger.info(f"Retrieving current rates at {url}")
                 try:
-                    with open(self.output_path, "w+") as f:
-                        json.dump(rates.to_dict(), f, indent=4)
+                    rates = await self.get_current_rates(url)
+
                 except Exception as e:
-                    logger.error(e)
+                    logger.error(f"Could not retrieve rates from ARK Web API: {e}")
                     await asyncio.sleep(self.polling_delay)
                     continue
 
-            last_rates = RatesStatus.from_dict(last_rates_dict)
-
-            # compare rates to last rates
-            rates_diff = rates.get_diff(last_rates)
-
-            if rates_diff.items:
-
-                # save rates to file
+                # get old rates from file
                 try:
-                    with open(self.output_path, "w+") as f:
-                        json.dump(rates.to_dict(), f, indent=4)
-                except Exception as e:
-                    logger.error(e)
-                    await asyncio.sleep(self.polling_delay)
-                    continue
+                    with open(output_path) as f:
+                        last_rates_dict = json.load(f)
+                except (FileNotFoundError, JSONDecodeError) as e:
+                    logger.warn(f"Problem loading file at {output_path}: {e}")
+                    last_rates_dict = copy.deepcopy(rates).to_dict()
+                    try:
+                        with open(output_path, "w+") as f:
+                            json.dump(rates.to_dict(), f, indent=4)
+                    except Exception as e:
+                        logger.error(e)
+                        await asyncio.sleep(self.polling_delay)
+                        continue
 
-                # generate and send embed
-                logger.info("Rates changed - sending embed")
-                embed_description = rates_diff.to_embed(rates)
-                await self.send_embed(embed_description)
+                last_rates = RatesStatus.from_dict(last_rates_dict)
+
+                # compare rates to last rates
+                rates_diff = rates.get_diff(last_rates)
+
+                if rates_diff.items:
+
+                    # save rates to file
+                    try:
+                        with open(output_path, "w+") as f:
+                            json.dump(rates.to_dict(), f, indent=4)
+                    except Exception as e:
+                        logger.error(e)
+                        await asyncio.sleep(self.polling_delay)
+                        continue
+
+                    # generate and send embed
+                    logger.info(f"Rates at {url} changed - sending embed")
+                    embed_description = rates_diff.to_embed(rates)
+                    await self.send_embed(embed_description)
 
             await asyncio.sleep(self.polling_delay)
 
