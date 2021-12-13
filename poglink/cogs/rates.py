12031234f57a15fc35cef15b323b86ff0ca88283
Travis Prosser <travisprosser@gmail.com>
2021-12-13 00:39:08 -0400

    fix: reverse new/old args when getting rates diff in cog

--- poglink/cogs/rates.py
+++ poglink/cogs/rates.py
@@ -143,8 +143,7 @@ class Rates(commands.Cog):
                 last_rates = RatesStatus.from_dict(last_rates_dict)
 
                 # compare rates to last rates
-                # TODO: I think this is backwards; it should be last_rates.get_diff(rates)
-                rates_diff = rates.get_diff(last_rates)
+                rates_diff = last_rates.get_diff(rates)
 
                 if rates_diff.items:
 
